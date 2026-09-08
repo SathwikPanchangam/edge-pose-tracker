48dfba342ba91c085b352a9b6d9bb3869657156b
SathwikPanchangam <sathwik.nagasai@gmail.com>
2026-09-08 11:00:00 +0530

    chore: initialize ROS 2 package scaffold and git tracking

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+
+package_name = 'edge_pose_tracker'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='sathwiksai30',
+    maintainer_email='sathwiksai30@todo.todo',
+    description='Edge-based 6D pose tracking and uncertainty quantification pipeline',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
